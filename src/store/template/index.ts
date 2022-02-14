c5ed970b869a761df23123f9918a7c787c143afe
Andres Melendez <codeslator@gmail.com>
2022-02-13 21:57:51 -0400

    fixing bugs before deploy

--- src/store/template/index.ts
+++ src/store/template/index.ts
@@ -1,11 +1,11 @@
 import { Module } from 'vuex';
-import { StateInterface } from '../index';
+import { StoreInterface } from '../index';
 import state, { ExampleStateInterface } from './state';
 import actions from './actions';
 import getters from './getters';
 import mutations from './mutations';
 
-const exampleModule: Module<ExampleStateInterface, StateInterface> = {
+const exampleModule: Module<ExampleStateInterface, StoreInterface> = {
     namespaced: true,
     actions,
     getters,
